98e92641c8b31b759d9cc9d607c9ca9e9d877bda
justin <justinmatt28@gmail.com>
2018-01-26 21:15:45 +0530

    code improvements

--- PendulumSim.py
+++ PendulumSim.py
@@ -1,4 +1,7 @@
-from Core.pendulum import pendulum
+
+__author__ = 'justin'
+
+from Modules.pendulum import pendulum
 from visual import *
 import sys  
 
@@ -10,11 +13,11 @@ def main():
     
     amplitude=input("Amplitude :")
     if amplitude > length :                                                #Maximum distance bob covers in x-axis cannot exceed the length of pendulum
-        print "Illegal value.Value of amplitude should be less",length
+        print "Illegal value.Value of amplitude should be less than ",length
         sys.exit()
  
   
-    pendulum(length,g,amplitude)
+    pendulum(length,g,amplitude)  #calling function from Modules 
 
 
 if __name__== '__main__':
